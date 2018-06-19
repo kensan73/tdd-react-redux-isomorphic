22c5f1948a0979d4487f0626aabbba5374fd1fc2
kensan73 <ken.60625@gmail.com>
2018-06-19 10:25:51 -0500

    add test for search button handling clicks

--- src/header/tests/SearchButton_spec.js
+++ src/header/tests/SearchButton_spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import expect from 'expect';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import sinon from 'sinon';
 
 import SearchButton from '../components/SearchButton';
@@ -8,21 +8,33 @@ import SearchButton from '../components/SearchButton';
 describe('SearchButton', () => {
   describe('SearchButton conditional display', () => {
     it('should render when there is a non-empty query', () => {
-      const searchButton = shallow(<SearchButton query="some query" />);
+      const searchButton = shallow(<SearchButton query="some query"/>);
 
       expect(searchButton.html()).toBe('<button class="search-button">Search</button>');
     });
 
     it('should not render when there is an empty query', () => {
-      const searchButton = shallow(<SearchButton query="" />);
+      const searchButton = shallow(<SearchButton query=""/>);
 
       expect(searchButton.html()).toBe(null);
     });
 
     it('should not render when there are just spaces for query', () => {
-      const searchButton = shallow(<SearchButton query="              " />);
+      const searchButton = shallow(<SearchButton query="              "/>);
 
       expect(searchButton.html()).toBe(null);
+    });
+  });
+
+  describe('SearchButton behavior', () => {
+    it('should call the onclick handler when button is clicked', () => {
+      let fakeFunction = sinon.spy();
+
+      const searchButton =
+        shallow(<SearchButton query="some query" handleClick={fakeFunction} />);
+      searchButton.simulate('click');
+
+      expect(fakeFunction.called).toBe(true);
     });
   });
 });
