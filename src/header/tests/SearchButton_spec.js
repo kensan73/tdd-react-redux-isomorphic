8c9e662912367536ca6ea7a33aff3b00a8093c6b
kensan73 <ken.60625@gmail.com>
2018-06-19 08:31:17 -0500

    create a search button test that just imports the sut

---
+++ src/header/tests/SearchButton_spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import expect from 'expect';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import SearchButton from '../components/SearchButton';
+
+describe('Button', () => {
+  describe('Button behavior', () => {
+  });
+});
