131bae1ff02b840079270a828f0efb075d5f4386
kensan73 <ken.60625@gmail.com>
2018-06-19 10:18:03 -0500

    add test for query having only spaces answer

--- src/header/components/SearchButton.jsx
+++ src/header/components/SearchButton.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class SearchButton extends Component {
   render() {
     const {query} = this.props;
-    if(query == '')
+    if(query.trim() == '')
       return null;
     return <button className="search-button">Search</button>;
   }
