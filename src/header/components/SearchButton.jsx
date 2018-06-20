76e23aa1aefcfdf042959538a3cbd10818ad6ad8
kensan73 <ken.60625@gmail.com>
2018-06-20 05:14:22 -0500

    add test for search button handling clicks answer

--- src/header/components/SearchButton.jsx
+++ src/header/components/SearchButton.jsx
@@ -1,10 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 export default class SearchButton extends Component {
   render() {
-    const {query, handleClick} = this.props;
-    if(query.trim() == '')
-      return null;
+    const { query, handleClick } = this.props;
+    if (query.trim() === '') { return null; }
     return <button onClick={handleClick} className="search-button">Search</button>;
   }
 }
